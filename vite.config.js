73a3d97d005ee0619e7c559a490765a52e002d1b
BuildTools <unconfigured@null.spigotmc.org>
2023-04-17 19:33:02 +0200

    Removed "@vitejs/plugin-basic-ssl" as there is no need for it now.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: { https: true }, // Needed for use of IndexedDB and OPFS
-	plugins: [sveltekit(), basicSsl()],
+	plugins: [sveltekit()],
 	build: {
 		target: 'esnext'
 	}
