3c9ac3642d5cd87e4d612675701ffb9ba17f2033
Wild <57220519+frkri@users.noreply.github.com>
2023-04-19 09:26:14 +0000

    change viewtype store localstorage name to be more detailed

--- src/lib/store/viewType.ts
+++ src/lib/store/viewType.ts
@@ -1,10 +1,9 @@
 import { writable } from 'svelte/store';
-
-// Create a Store that also writes to localStorage
-// NOTE: LocalStorage only stores strings!
-export const viewType = writable<boolean>(localStorage.getItem('prefers-viewType') === 'true');
+// Creates a Store that also writes to localStorage
+// NOTE: LocalStorage only stores strings, meaning we need to convert the bool to string!
+export const viewType = writable<boolean>(localStorage.getItem('prefers-viewType-grid') === 'true');
 
 // On update, write to localStorage
 viewType.subscribe((value) => {
-	localStorage.setItem('prefers-viewType', value.toString());
+	localStorage.setItem('prefers-viewType-grid', value.toString());
 });
