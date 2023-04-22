5e55d9b8f12085df5f11a7a0ae6327dbe17e0a4a
frkri <unconfigured@null.spigotmc.org>
2023-04-22 23:14:04 +0200

    Improve Modal

--- src/lib/components/popup/actionModal.svelte
+++ src/lib/components/popup/actionModal.svelte
@@ -1,17 +1,39 @@
 <script lang="ts">
-	export let open = false;
+	import Portal from 'svelte-portal';
 
-	// optional title
+	export let open = false;
 	export let title = '';
+
+	$: if (open) {
+		document.addEventListener('keydown', handleKeyDown, true);
+	} else {
+		document.removeEventListener('keydown', handleKeyDown, true);
+	}
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key === 'Escape') {
+			open = false;
+		}
+	}
 </script>
 
 {#if open}
-	<div class="fixed bottom-0 left-0 flex w-screen justify-center p-5">
-		<div class="border-main flex h-56 flex-col justify-around p-3 md:w-1/2">
-			{#if title}
-				<h2 class="border-b text-xl font-bold">{title}</h2>
-			{/if}
-			<slot />
+	<Portal target="#portal-target">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="fixed bottom-0 left-0 flex w-screen h-screen justify-center items-end p-5 z-10 bg-background bg-opacity-40"
+			on:click={(e) => {
+				if (e.target === e.currentTarget) {
+					open = false;
+				}
+			}}
+		>
+			<div class="border-main flex h-56 flex-col justify-around p-3 md:w-1/2 bg-background">
+				{#if title}
+					<h2 class="border-b text-xl font-bold">{title}</h2>
+				{/if}
+				<slot />
+			</div>
 		</div>
-	</div>
+	</Portal>
 {/if}
