2d331be24d5bb5135415e074306df839b62c2118
frkri <57220519+frkri@users.noreply.github.com>
2023-05-13 17:51:55 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="mx-auto flex w-80 flex-col items-center justify-center gap-5">
+	<p class="text-6xl font-bold">
+		{$page.status}
+	</p>
+	<h1 class="text-xl font-bold">An error has occurred!</h1>
+	<p>
+		{$page.error?.message}
+	</p>
+	<a href="/explorer" class="button normal">Return to Explorer</a>
+</div>
